53956db4cb2faa4b88223822b34d80d83ff6be2c
BorisGond <boris_gondalev@mail.ru>
2023-11-09 22:18:53 +0300

    algorithms

---
+++ zadachi/algorithm(365-376)/main.js
@@ -0,0 +1,77 @@
+// let elem = document.getElementById("elem");
+// let value = elem.getAttribute('value');
+
+// console.log(value)
+
+// let elem1 = document.getElementById("elem1");
+// let classs = elem1.getAttribute("class");
+
+// console.log(classs);
+
+//#2
+// let elem = document.getElementById("elem");
+// let value = elem.setAttribute('value', 'text');
+
+// let elem1 = document.getElementById("elem1");
+// let classs = elem1.setAttribute('class', 'valid');
+
+//#3
+// let elem = document.getElementById('elem');
+// elem.removeAttribute('value');
+
+//#4
+// let elem = document.querySelector('#elem')
+
+// elem.addEventListener('click', ()=> elem.textContent += elem.dataset.text);
+
+
+// let elem = document.querySelectorAll('div')
+
+// elem.forEach(elem => {
+//     elem.addEventListener('click', ()=> elem.textContent += elem.dataset.num)
+// });
+
+//#5
+// let elem = document.querySelector('#elem')
+
+// elem.addEventListener('click', ()=> elem.textContent += elem.dataset.productPrice * elem.dataset.productAmount  + " ")
+
+//#6
+// let elem = document.querySelectorAll('p');
+
+// elem.forEach(element => {
+//     for (let i = 0; i < elem.length; i++) {
+//         elem[i].setAttribute('data-num', i + 1);
+//     }
+    
+// });
+//#7
+// let elem = document.getElementById('elem');
+        
+// let length = elem.classList.length;
+// console.log(length);
+
+// let elem1 = document.getElementById('elem1');
+// let classNames = elem1.classList;
+
+// for (let className of classNames) {
+// 	console.log(className);
+// }
+
+//#8
+// let elem = document.getElementById('elem');
+//elem.classList.add('xxx');
+
+// #9
+// let elem = document.getElementById('elem');
+// elem.classList.remove('zzz');
+
+// 10
+// let elem = document.getElementById('elem');
+
+// let contains = elem.classList.contains('ggg');
+// console.log(contains);
+
+// 11
+// let elem = document.getElementById('elem');
+// elem.classList.toggle('zzz');
